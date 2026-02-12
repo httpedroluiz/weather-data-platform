c2047b3964bab94bdb50e922850c9f6a5b151f15
Pedro Luiz <pedroluizmizael@gmail.com>
2026-02-11 23:14:44 -0300

    feat(frontend): setup react + vite + tailwind base structure

---
+++ frontend/src/pages/Dashboard.tsx
@@ -0,0 +1,13 @@
+export function Dashboard() {
+  return (
+    <div className="p-8">
+      <h2 className="text-2xl font-bold mb-4">
+        Dashboard
+      </h2>
+
+      <div className="bg-gray-800 p-6 rounded-xl">
+        Em breve: dados do clima
+      </div>
+    </div>
+  );
+}
